ae5bc7eba91fe7b2cfd274522111d298aa003a8a
thomas <thomas@maschine>
2019-10-14 21:10:10 +0200

    'ä' added as vocal

--- PasswordGenerator/src/impl/PasswordGenerator.java
+++ PasswordGenerator/src/impl/PasswordGenerator.java
@@ -2,7 +2,7 @@ package impl;
 
 public interface PasswordGenerator {
 
-	public static final char[] VOCALS = new char[] {'a','e','i','o','u'};
+	public static final char[] VOCALS = new char[] {'a','e','i','o','u','ä'};
 	public static final char[] CONSONANTS = new char[] {'b','c','d','f','g','h',
 			'j','k','l','m','n','p','q','q','r','s','t','v','w','x','y','z'};
 	public static final int length = 10;  
